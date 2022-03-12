017ac94ba50387f789e6d7f8791e68b2f883eaca
Hakan Ozkok <hakanozkok@gmail.com>
2022-03-12 18:23:24 +0000

    added zoom modal to gallery images

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+const images = document.querySelectorAll(".gallery-container img");
+let imgSrc;
+// get images src onclick
+images.forEach((img) => {
+    img.addEventListener("click", (e) => {
+        imgSrc = e.target.src;
+        //run modal function
+        imgModal(imgSrc);
+    });
+});
+//creating the modal
+let imgModal = (src) => {
+  const modal = document.createElement("div");
+  modal.setAttribute("class", "modal");
+  //add the modal to the main section or the parent element
+  document.querySelector(".main").append(modal);
+  //adding image to modal
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", src);
+  //creating the close button
+  const closeBtn = document.createElement("button");
+  closeBtn.setAttribute("class", "btn-close btn-close-white closeBtn");
+  closeBtn.setAttribute("type", "button");
+  closeBtn.setAttribute("aria-label", "Close");
+  //close function
+  closeBtn.onclick = () => {
+      modal.remove();
+  };
+  modal.onclick = ({ target }) => {
+    if (target === modal) {
+      modal.remove();
+    }
+  }
+  let isZoomed = false;
+  newImage.onclick = () => {
+    if (!isZoomed) {
+      modal.classList.add("modal-zoomed");
+      isZoomed = true;
+    } else {
+      modal.classList.remove("modal-zoomed");
+      isZoomed = false;
+    }
+  };
+  modal.append(newImage, closeBtn);
+};
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode == 27) {
+    const modal = document.querySelector(".modal");
+    if (modal !== null) {
+      modal.remove();
+    }
+  }
+})
