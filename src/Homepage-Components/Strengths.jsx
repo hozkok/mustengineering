e93274fe9432a200acca9f0c49d3e258e148173e
hozkok <hozkok@LAPTOP-7PIDK1VL.localdomain>
2025-08-09 19:44:01 +0100

    fix link once more

--- src/Homepage-Components/Strengths.jsx
+++ src/Homepage-Components/Strengths.jsx
@@ -103,7 +103,7 @@ function Strengths() {
               <li>Value-Driven Solutions</li>
             </ol>
             <img
-              src="whyMustEng/picture40.webp"
+              src="whyMustEng/Picture40.webp"
               alt=""
               className="w-full shadow-elevation-high rounded-md lg:w-1/2 place-self-center"
             />
